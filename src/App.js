855bd6c6c0cb49c70df8542e3db9054edc7e1c55
Sunga Thawethe <sungabt@gmail.com>
2023-06-13 18:46:04 +0200

    Update according to Optional Suggestion - Create component for Navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
 import './index.css';
 import Home from './components/Home';
+import NavLinks from './components/NavLinks';
 
 function App() {
   return (
     <>
-      <nav>
-        <h1 className="page-title">Math Magicians</h1>
-        <ul className="nav-list">
-          <li>
-            <Link className="link" to="/">Home</Link>
-          </li>
-          <li>
-            <Link className="link" to="/Calculator">Calculator</Link>
-          </li>
-          <li>
-            <Link className="link q-link" to="/Quotes">Quotes</Link>
-          </li>
-        </ul>
-      </nav>
+      <NavLinks />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Calculator" element={<Calculator />} />
